b0a21e280059257039e6c07fb5e80d8f99c02047
jadamhunt <justin.adam.hunt@gmail.com>
2026-04-13 13:40:27 -0700

    Update setup.sh
    Fixed MAC address pull for multiple interfaces

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,8 @@
 #=========================#
 # Gather IP and MAC Addr from ip binary
 IPADDR=$(ip a|grep /24| awk '{print $2}'| cut -d"/" -f 1)
-MACADDR=$(ip a|grep ether| awk '{print $2}')
+#MACADDR=$(ip a|grep ether| awk '{print $2}')
+MACADDR=$(ip a | grep enp0 -A 1 | grep ether | awk '{print $2}')
 # point to a hostdb file in home directory
 HOSTDB=/home/$USER/hostdb
 
@@ -23,19 +24,20 @@ echo "Current Configurations:"
 echo "#=========================#"
 echo "IP Address: $IPADDR"
 echo "MAC Address: $MACADDR"
-echo "#=========================# \n"
+echo "#=========================#"
 echo "Checking to see if HostnameDB exists..."
+sleep 1
 
 # Checking to see if HostDB file exists...
 if test -f $HOSTDB; then
     # Found the hostdb file and pulling the hostname
     # assumes proper formatting and records hostname to var
     echo "Found hostdb"
-    HOSTNM=$(cat hostdb| grep $MACADDR | cut -d"," -f 4)
-    echo "FOund MAC address in hostdb:" $HOSTNM
+    HOSTNM=$(cat ~/hostdb| grep $MACADDR | cut -d"," -f 4)
+    echo "Found MAC address in hostdb:" $HOSTNM
     echo $HOSTNM
     # Resetting hostname
-    $(hostnamectl hostname $HOSTNM)
+    $(sudo hostnamectl hostname $HOSTNM)
 fi
 #@TODO cleanup hostdb file
 #@TODO remediation if hostdb file isnt found.
